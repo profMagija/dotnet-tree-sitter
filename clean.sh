2e9a19b956e254c32b615815e9c156bf57ccb8f9
Nikola Bebić <nikola.bebic99@gmail.com>
2019-12-21 10:28:15 +0100

    more changes

--- clean.sh
+++ clean.sh
@@ -1,1 +1,5 @@
-rm -f TreeSitter/tree-sitter.so TreeSitter.C/tree-sitter-c.so TreeSitter.JavaScript/tree-sitter-javascript.so
+rm -f \
+    TreeSitter/tree-sitter.so \
+    TreeSitter.C/tree-sitter-c.so \
+    TreeSitter.JavaScript/tree-sitter-javascript.so \
+    TreeSitter.Python/tree-sitter-python.so
